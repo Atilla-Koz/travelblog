b0880e9b1ece2c85de7deba5b13c435ac2b45344
Atilla Köz <115349531+Atilla-Koz@users.noreply.github.com>
2025-02-07 21:21:09 +0300

    Add ScrollToTop component and Instagram link to LanguageToggle

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import About from './pages/About';
 import Explore from './pages/Explore';
 import Journal from './pages/Journal';
 import { LanguageProvider } from './context/LanguageContext.jsx';
+import ScrollToTop from './components/ScrollToTop';
 import './index.css';
 
 function App() {
   return (
     <LanguageProvider>
       <Router>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
